2573be7e57de1b2c34e6246df7b4549b48fd556c
MeowSamOP-lyrics <oggs686@gmail.com>
2025-02-18 21:37:54 +0530

    qBedwars

---
+++ webmanifest.php
@@ -0,0 +1,35 @@
+{
+    "name": "Hypixel Forums",
+    "short_name": "Hypixel",
+    "description": "Play award winning Minecraft games and mini-games with your friends for free. Only on the Hypixel Minecraft server!",
+    "icons": [
+        {
+            "src": "https:\/\/hypixel.net\/styles\/hypixel-v2\/images\/hypixel-192px.png",
+            "sizes": "192x192",
+            "purpose": "any"
+        },
+        {
+            "src": "https:\/\/hypixel.net\/styles\/hypixel-v2\/images\/hypixel-192px.png",
+            "sizes": "192x192",
+            "purpose": "maskable"
+        },
+        {
+            "src": "https:\/\/hypixel.net\/styles\/hypixel-v2\/images\/hypixel-512px.png",
+            "sizes": "512x512",
+            "purpose": "any"
+        },
+        {
+            "src": "https:\/\/hypixel.net\/styles\/hypixel-v2\/images\/hypixel-512px.png",
+            "sizes": "512x512",
+            "purpose": "maskable"
+        }
+    ],
+    "lang": "en-US",
+    "dir": "ltr",
+    "display": "standalone",
+    "scope": "\/",
+    "id": "\/?_pwa=1",
+    "start_url": "\/?_pwa=1",
+    "background_color": "#0b0a07",
+    "theme_color": "#0b0a07"
+}
